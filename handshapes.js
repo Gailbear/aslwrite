01bc9ed4eb4b43f0c28bb205d2c5f114b6e9abcb
Gail Terman <Gailbear@users.noreply.github.com>
2020-02-03 09:35:43 -0500

    update handshapes.js

--- handshapes.js
+++ handshapes.js
@@ -6,3 +6,21 @@ var thumb_in_r = "M 10 85 l 55 -30"
 var thumb_in_l = "M 5 55 l 55 30"
 var pointer_r = "M 35 5 v 90 m -15 -5 l 30 -25"
 var pointer_l = "M 35 5 v 90 m 15 -5 l -30 -25"
+
+var draw = SVG()
+function make_path(path_str) {
+
+  return draw.path(path_str).fill("none").stroke(width: "4")
+}
+
+var v_outside_path = make_path(v_outside)
+var v_inside_path = make_path(v_inside)
+var thumb_out_r_path = make_path(thumb_out_r)
+var thumb_out_l_path = make_path(thumb_out_l)
+
+var right_hand5 = draw.group().add(v_outside_path).add(v_inside_path).add(thumb_out_r_path)
+
+var left_hand5 = draw.group().add(v_outside_path).add(v_inside_path).add(thumb_out_l_path)
+
+var right = {hand5: right_hand5}
+var left = {hand5: left_hand5}
